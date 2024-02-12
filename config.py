4860e1fc37786586520715a767e59e8e33938e2c
raul-theion <raul@theion.de>
2024-02-12 14:56:51 +0100

    - Add scpi device library, add config file, wirte better functionality   example inside main().

---
+++ config.py
@@ -0,0 +1,4 @@
+
+
+
+serial_port = "COM12"
--- ea_ps_2042.py
+++ ea_ps_2042.py
@@ -1,7 +1,4 @@
 
-
-
-
 '''
 
 This script will handle setting and getting the ABSOLUTE MINIMUM parameters required to control the CNT alignment system.
@@ -9,15 +6,23 @@ Different lab power supplies may be used, but which one is used will be abstract
 
 '''
 
+# standard python libraries #
 import logging
 logging.basicConfig(level=logging.WARNING)		# enable debug messages
-
 import time
 import sys
 
+# pip installed libraries #
 # import serial           # the function generator works with serial port communication (over USB)
 
-from scpi_serial_device import scpi_serial_device                # parent class to inherit from in the case of the supply and function generators
+# custom submodule libraries #
+
+from soft_lib_scpi_device.scpi_serial_device import scpi_serial_device
+
+# custom project libraries #
+import config as config
+
+# from scpi_serial_device import scpi_serial_device                # parent class to inherit from in the case of the supply and function generators
 
 class power_supply(scpi_serial_device):
 
@@ -102,38 +107,48 @@ class power_supply(scpi_serial_device):
         response = float(response)      # convert to floating number
         return(response)
 
+def test_sweep():
+    for i in range(1,20):
+        t = 1
+        supply.set_output("on")
+        time.sleep(t)
+        supply.set_output("off")
+        supply.set_volt(i)
+        print("voltage set to " + str(i))
+        supply.set_current(i/10)
+        print("current set to " + str(i/10))
+        time.sleep(t)
+
+
 # MAIN FUNCTION ########################################################
 
 if __name__ == "__main__":
 
-    t = 0.1
-
     supply = power_supply()
-    supply.serial_connect("COM3")
+    supply.serial_connect(config.serial_port)
     print("Device id confirmation:")
     print(supply.confirm_device_id())
+    time.sleep(1)
+    print("resetting power supply")
     supply.reset()
-    supply.reset()
-    supply.reset()
-    supply.reset()
+    time.sleep(1)
+
+    print("setting 'default' values: (required by minipuls back then)")
     supply.set_default_values()
+    time.sleep(1)
 
+    print("switching on: ")
     supply.set_output("on")
+    time.sleep(1)
 
+    print("current voltage:")
     print(supply.get_volt())
+    print("current intensity:")
     print(supply.get_current())
+    time.sleep(1)
 
-    time.sleep(2)
+    print("Varying voltage and current max vals.")
+    test_sweep()
 
+    print("switching off:")
     supply.set_output("off")
-
-    # for i in range(1,20):
-    #     supply.set_output("on")
-    #     time.sleep(t)
-    #     supply.set_output("off")
-    #     supply.set_volt(i)
-    #     supply.set_current(i/10)
-    #     time.sleep(t)
-
-
-    #funcgen.set_offset()
